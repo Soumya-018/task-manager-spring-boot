06bdc090e0c98a751f458127e83a34d546a93685
Soumya Ranjan Badajena <soumyaranjanbadajena0@gmail.com>
2026-09-21 23:52:47 +0530

    refactor: rename key generator class

--- src/main/java/com/soumya/taskmanager/KeyGenerator.java
+++ src/main/java/com/soumya/taskmanager/KeyGenerator.java
@@ -4,7 +4,7 @@ import io.jsonwebtoken.Jwts;
 
 import java.util.Base64;
 
-public class keyGenerator {
+public class KeyGenerator {
     public static void main(String[] args) {
         String secret = Base64.getEncoder().encodeToString(Jwts.SIG.HS256.key().build().getEncoded());
         System.out.println(secret);
